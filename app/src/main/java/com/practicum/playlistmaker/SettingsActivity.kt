fa4d51da7021de5d21185ee211b3f904851e8308
AndroidMaster <ya-mihim@yandex.ru>
2024-07-27 01:17:12 +0300

    28_share_button

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +15,17 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        //sharing
+        val button_share = findViewById<Button>(R.id.button_share)
+        button_share.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.share_message_subject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, R.string.share_message)
+            //shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Полезная ссылка:")
+            //shareIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/profile/android-developer/")
+            startActivity(shareIntent)
+        }
     }
 
 }
