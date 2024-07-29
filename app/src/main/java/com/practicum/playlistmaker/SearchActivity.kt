a15691507ab49f28966198e66416ef2e1b41fdd4
Michel-mihim <ya-mihim@yandex.ru>
2024-07-29 11:43:29 +0300

    38_textWatch_clear_button_visibility

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,8 +1,14 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,5 +32,39 @@ class SearchActivity : AppCompatActivity() {
             startActivity(search_back_intent)
         }
 
+        //editText
+        val search_editText = findViewById<EditText>(R.id.search_editText)
+        val search_clear_button = findViewById<ImageButton>(R.id.search_clear_button)
+
+        search_clear_button.visibility = View.GONE
+
+        search_clear_button.setOnClickListener {
+            search_editText.setText(getString(R.string.empty_string))
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                search_clear_button.visibility = searchClearButtonVisibility(s)
+                //Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+        }
+
+        search_editText.addTextChangedListener(textWatcher)
+    }
+
+    private fun searchClearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
