441cb3cd5f1d0a189225cba559f168797a6fee8f
AndroidMaster <ya-mihim@yandex.ru>
2024-09-08 10:59:33 +0300

    88_search_history_prefs_added

--- app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
@@ -1,7 +1,22 @@
 package com.practicum.playlistmaker
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
+
+const val SEARCH_HISTORY_KEY = "history"
 
 class SearchHistory(sharedPrefs: SharedPreferences) {
+
+    fun writeHistory(sharedPrefs: SharedPreferences, track: Track) {
+        val json = Gson().toJson(track)
+        sharedPrefs.edit()
+            .putString(SEARCH_HISTORY_KEY, json)
+            .apply()
+    }
+
+    fun readHistory(sharedPrefs: SharedPreferences):Track? {
+        val json = sharedPrefs.getString(SEARCH_HISTORY_KEY, null) ?: return null
+        return Gson().fromJson(json, Track::class.java)
+    }
 
 }
