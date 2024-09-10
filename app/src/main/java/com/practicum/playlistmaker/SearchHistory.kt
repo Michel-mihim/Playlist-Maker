f41a72093cb9131f835183bf22f8f1b6716d7cb1
Michel-mihim <ya-mihim@yandex.ru>
2024-09-10 11:20:08 +0300

    98_present_track_move_making

--- app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.SharedPreferences
+import android.util.Log
 import android.widget.Toast
 import com.google.gson.Gson
 
@@ -14,7 +15,7 @@ class SearchHistory(val sharedPrefs: SharedPreferences) {
 
         val lastTracks = readHistory()
         newTracks.clear()
-        newTracks.addAll(historyVerifying(trackAdded, lastTracks, 10))
+        newTracks.addAll(historyNewReady(trackAdded, lastTracks, 10))
 
         val json = Gson().toJson(newTracks)
         this.sharedPrefs.edit()
@@ -33,21 +34,38 @@ class SearchHistory(val sharedPrefs: SharedPreferences) {
             .apply()
     }
 
-    fun historyVerifying(trackAdded: Track, tracks: Array<Track>, maxTracksCount: Int): ArrayList<Track> {
-
-        var idsList = ArrayList<String>()
-        tracks.forEach { track ->
-           idsList.add(track.trackId)
+    fun historyNewReady(trackAdded: Track, tracks: Array<Track>, maxTracksCount: Int): ArrayList<Track> {
+        var tracksNewReady = ArrayList<Track>()
+        if (trackListChecker(getTrackId(trackAdded), tracks)) {
+            Log.d("WTF", "Есть такой. Будет перемещен на первое место")
+            return moveTrackIfPresent(trackAdded, tracks)
+        }
+        Log.d("WTF", "Нет такого. Будет добавлен")
+        return addTrackIfAbsent(trackAdded, tracks)
         }
 
-        if (trackAdded.trackId in idsList) {
-
-        } else {
-
-        }
-        return
+    fun getTrackId(track: Track): String {
+        return track.trackId
     }
 
-    fun isInList():
+    fun trackListChecker(trackId: String, tracks: Array<Track>): Boolean {
+        for (track in tracks) {
+            if (getTrackId(track) == trackId) {
+                return true
+            }
+        }
+        return false
+    }
 
+    fun addTrackIfAbsent(trackAdded: Track, tracks: Array<Track>): ArrayList<Track> {
+        var tracksReady = ArrayList<Track>()
+        tracksReady.add(trackAdded)
+        tracksReady.addAll(tracks)
+        return tracksReady
+    }
+
+    fun moveTrackIfPresent(trackAdded: Track, tracks: Array<Track>): ArrayList<Track> {
+
+        return
+    }
 }
