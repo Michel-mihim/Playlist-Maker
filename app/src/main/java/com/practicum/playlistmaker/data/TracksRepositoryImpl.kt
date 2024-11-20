81bcf7a2f536a122192a5eda76cb5051958cc6e0
AndroidMaster <ya-mihim@yandex.ru>
2024-11-20 22:53:02 +0300

    144_repositoryImpl

---
+++ app/src/main/java/com/practicum/playlistmaker/data/TracksRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.practicum.playlistmaker.data
+
+import com.practicum.playlistmaker.data.dto.TracksSearchRequest
+import com.practicum.playlistmaker.data.dto.TracksSearchResponse
+import com.practicum.playlistmaker.domain.api.TracksRepository
+import com.practicum.playlistmaker.domain.models.Track
+
+class TracksRepositoryImpl(private val networkClient: NetworkClient) : TracksRepository {
+    override fun searchTracks(expression: String): List<Track> {
+        val response = networkClient.doRequest(TracksSearchRequest(expression))
+        if (response.resultCode == 200) {
+            return (response as TracksSearchResponse).results.map {Track(
+                it.trackId,
+                it.trackName,
+                it.artistName,
+                it.trackTimeMillis,
+                it.artworkUrl100,
+                it.collectionName,
+                it.releaseDate,
+                it.primaryGenreName,
+                it.country,
+                it.previewUrl
+            )}
+        } else {
+            return emptyList()
+        }
+    }
+}
