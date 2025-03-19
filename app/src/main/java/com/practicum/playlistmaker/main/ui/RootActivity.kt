343a47ce77ac980fb74ce716b59523f9c392e692
AndroidMaster <ya-mihim@yandex.ru>
2025-03-19 22:02:47 +0300

    RootActivityBinding_added

--- app/src/main/java/com/practicum/playlistmaker/main/ui/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/main/ui/RootActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
